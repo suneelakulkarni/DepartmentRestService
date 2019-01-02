be2d7b031c1a539add0c00a7811c7031f79f1b90
Sunil Kulkarni <sunilkulkarni@Sunils-MacBook-Pro.local>
2019-01-02 00:15:23 -0800

    Initial Spring boot app

---
+++ src/test/java/com/rest/department/DepartmentApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rest.department;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepartmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
